c90ba7a24d3a56e2ba7af6153bb4c29b7378221d
Miroslav Rezanina <mrezanin@redhat.com>
2013-11-06 12:36:03 +0100

    monitor: Remove usb_add/del commands for Red Hat Enterprise Linux
    RH-Author: Miroslav Rezanina <mrezanin@redhat.com>
    Message-id: <daf2ca0042519cd01a6a68d30eb76bdd8b88c9f2.1383741033.git.mrezanin@redhat.com>
    Patchwork-id: 55520
    O-Subject: [RHEL7 qemu-kvm PATCH v2 3/4] monitor: Remove usb_add/del commands for Red Hat Enterprise Linux
    Bugzilla: 1010858
    RH-Acked-by: Michal Novotny <minovotn@redhat.com>
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Orit Wasserman <owasserm@redhat.com>
    RH-Acked-by: Eric Blake <eblake@redhat.com>
    
    From: Miroslav Rezanina <mrezanin@redhat.com>
    
    This is forwardport of RHEL-6 commit 754e6292b4ab41c8848171555af830ab7284f4d3:
    
        monitor: Remove usb_add/del commands for Red Hat Enterprise Linux
    
        The usb_add/del commands have been obsoleted by the device_add/del
        commands.
    
        Signed-off-by: Amit Shah <amit.shah@redhat.com>
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    (cherry picked from commit 567d99ac6c3ce941ad5ec3e09b1698eaefffd4e2)
    (cherry picked from commit d9922163a646af496445f923164d9efaeb3a7960)

--- hmp-commands.hx
+++ hmp-commands.hx
@@ -647,6 +647,7 @@ STEXI
 Compute the checksum of a memory region.
 ETEXI
 
+#if 0 /* Disabled for Red Hat Enterprise Linux */
     {
         .name       = "usb_add",
         .args_type  = "devname:s",
@@ -677,6 +678,7 @@ Remove the USB device @var{devname} from the QEMU virtual USB
 hub. @var{devname} has the syntax @code{bus.addr}. Use the monitor
 command @code{info usb} to see the devices you can remove.
 ETEXI
+#endif
 
     {
         .name       = "device_add",
