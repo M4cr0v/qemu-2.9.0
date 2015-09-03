ed160d96655f98cdce1f7f7ded5b47599bda14c8
David Gibson <dgibson@redhat.com>
2015-09-03 04:09:04 +0200

    RHEL: Set vcpus hard limit to 240 for Power
    Patchwork-id: 67655
    O-Subject: [RHEL7.2 qemu-kvm-rhev PATCH] RHEL: Set vcpus hard limit to 240 for Power
    Bugzilla: 1257781
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Thomas Huth <thuth@redhat.com>
    RH-Acked-by: Laszlo Ersek <lersek@redhat.com>
    
    On POWER, the kernel advertises a soft limit based on the number of CPU
    threads on the host.  We want to allow exceeding this for testing purposes,
    so we don't want to set hard limit to soft limit as on x86.
    
    However the POWER hard limit advertised by the kernel is 2048 (== NR_CPUS)
    but we only want to allow up to the number of vCPUs we actually test, so
    we force the hard limit to 240.
    
    Signed-off-by: David Gibson <dgibson@redhat.com>
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>
    (cherry picked from commit 8329392ab135fcaaa7fed94a6e8068dbed0ca473)
    (cherry picked from commit 598c434cf95391ca5a3ddc0e955221e227bf3344)

--- kvm-all.c
+++ kvm-all.c
@@ -1633,8 +1633,27 @@ static int kvm_init(MachineState *ms)
     soft_vcpus_limit = kvm_recommended_vcpus(s);
     hard_vcpus_limit = kvm_max_vcpus(s);
 
+#ifdef HOST_PPC64
+    /*
+     * RHEL hack:
+     *
+     * On POWER, the kernel advertises a soft limit based on the
+     * number of CPU threads on the host.  We want to allow exceeding
+     * this for testing purposes, so we don't want to set hard limit
+     * to soft limit as on x86.
+     *
+     * However the POWER hard limit advertised by the kernel is 2048
+     * (== NR_CPUS) but we only want to allow up to the number of
+     * vCPUs we actually test, so we force the hard limit to 240
+     */
+    hard_vcpus_limit = 240;
+    if (soft_vcpus_limit > hard_vcpus_limit) {
+        soft_vcpus_limit = hard_vcpus_limit;
+    }
+#else
     /* RHEL doesn't support nr_vcpus > soft_vcpus_limit */
     hard_vcpus_limit = soft_vcpus_limit;
+#endif
 
     while (nc->name) {
         if (nc->num > soft_vcpus_limit) {
