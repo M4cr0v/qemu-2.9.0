d7d8cefa12dbbfad4c7be5c1bf56f190d418bc57
Gerd Hoffmann <kraxel@redhat.com>
2017-10-20 08:34:38 +0200

    vga: handle cirrus vbe mode wraparounds.
    RH-Author: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: <20171020083439.32712-4-kraxel@redhat.com>
    Patchwork-id: 77396
    O-Subject: [RHV-7.4 qemu-kvm-rhev PATCH 3/4] vga: handle cirrus vbe mode wraparounds.
    Bugzilla: 1501300
    RH-Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    RH-Acked-by: Laurent Vivier <lvivier@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    
    Commit "3d90c62548 vga: stop passing pointers to vga_draw_line*
    functions" is incomplete.  It doesn't handle the case that the vga
    rendering code tries to create a shared surface, i.e. a pixman image
    backed by vga video memory.  That can not work in case the guest display
    wraps from end of video memory to the start.  So force shadowing in that
    case.  Also adjust the snapshot region calculation.
    
    Can trigger with cirrus only, when programming vbe modes using the bochs
    api (stdvga, also qxl and virtio-vga in vga compat mode) wrap arounds
    can't happen.
    
    Fixes: CVE-2017-13672
    Fixes: 3d90c6254863693a6b13d918d2b8682e08bbc681
    Cc: P J P <ppandit@redhat.com>
    Reported-by: David Buchanan <d@vidbuchanan.co.uk>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: 20171010141323.14049-3-kraxel@redhat.com
    (cherry picked from commit 28f77de26a4f9995458ddeb9d34bb06c0193bdc9)
    
    [ rhel7: differences due to 2.9 not having memory snapshot support ]
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- hw/display/vga.c
+++ hw/display/vga.c
@@ -1457,12 +1457,12 @@ static void vga_draw_graphic(VGACommonState *s, int full_update)
     DisplaySurface *surface = qemu_console_surface(s->con);
     int y1, y, update, linesize, y_start, double_scan, mask, depth;
     int width, height, shift_control, bwidth, bits;
-    ram_addr_t page0, page1, page_min, page_max;
+    ram_addr_t page0, page1, page_min, page_max, region_start, region_end;
     int disp_width, multi_scan, multi_run;
     uint8_t *d;
     uint32_t v, addr1, addr;
     vga_draw_line_func *vga_draw_line = NULL;
-    bool share_surface;
+    bool share_surface, force_shadow = false;
     pixman_format_code_t format;
 #ifdef HOST_WORDS_BIGENDIAN
     bool byteswap = !s->big_endian_fb;
@@ -1477,6 +1477,15 @@ static void vga_draw_graphic(VGACommonState *s, int full_update)
 
     s->get_resolution(s, &width, &height);
     disp_width = width;
+
+    region_start = (s->start_addr * 4);
+    region_end = region_start + s->line_offset * height;
+    if (region_end > s->vbe_size) {
+        /* wraps around (can happen with cirrus vbe modes) */
+        region_start = 0;
+        region_end = s->vbe_size;
+        force_shadow = true;
+    }
 
     shift_control = (s->gr[VGA_GFX_MODE] >> 5) & 3;
     double_scan = (s->cr[VGA_CRTC_MAX_SCAN] >> 7);
@@ -1517,7 +1526,7 @@ static void vga_draw_graphic(VGACommonState *s, int full_update)
     format = qemu_default_pixman_format(depth, !byteswap);
     if (format) {
         share_surface = dpy_gfx_check_format(s->con, format)
-            && !s->force_shadow;
+            && !s->force_shadow && !force_shadow;
     } else {
         share_surface = false;
     }
@@ -1633,10 +1642,19 @@ static void vga_draw_graphic(VGACommonState *s, int full_update)
             addr = (addr & ~0x8000) | ((y1 & 2) << 14);
         }
         update = full_update;
-        page0 = addr;
-        page1 = addr + bwidth - 1;
-        update |= memory_region_get_dirty(&s->vram, page0, page1 - page0,
-                                          DIRTY_MEMORY_VGA);
+        page0 = addr & s->vbe_size_mask;
+        page1 = (addr + bwidth - 1) & s->vbe_size_mask;
+        if (page1 < page0) {
+            /* scanline wraps from end of video memory to the start */
+            assert(force_shadow);
+            update |= memory_region_get_dirty(&s->vram, page0, 0,
+                                              DIRTY_MEMORY_VGA);
+            update |= memory_region_get_dirty(&s->vram, page1, 0,
+                                              DIRTY_MEMORY_VGA);
+        } else {
+            update |= memory_region_get_dirty(&s->vram, page0, page1 - page0,
+                                              DIRTY_MEMORY_VGA);
+        }
         /* explicit invalidation for the hardware cursor */
         update |= (s->invalidated_y_table[y >> 5] >> (y & 0x1f)) & 1;
         if (update) {
