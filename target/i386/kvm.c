ab77e1122be016fa22f99f7a4a5562b73d374661
Paolo Bonzini <pbonzini@redhat.com>
2017-12-13 15:48:11 -0200

    target-i386: add support for SPEC_CTRL MSR
    RH-Author: Eduardo Habkost <ehabkost@redhat.com>
    Message-id: <20171213174813.21283-2-ehabkost@redhat.com>
    Patchwork-id: n/a
    O-Subject: CONFIDENTIAL][RHEL-7.4.z qemu-kvm-rhev PATCH v2 1/3] target-i386: add support for
     SPEC_CTRL MSR
    Bugzilla: CVE-2017-5715
    RH-Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    RH-Acked-by: Miroslav Rezanina <mrezanin@redhat.com>
    RH-Acked-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- target/i386/kvm.c
+++ target/i386/kvm.c
@@ -90,6 +90,7 @@ static bool has_msr_hv_runtime;
 static bool has_msr_hv_synic;
 static bool has_msr_hv_stimer;
 static bool has_msr_xss;
+static bool has_msr_spec_ctrl;
 
 static bool has_msr_architectural_pmu;
 static uint32_t num_architectural_pmu_counters;
@@ -1141,6 +1142,10 @@ static int kvm_get_supported_msrs(KVMState *s)
                     has_msr_hv_stimer = true;
                     continue;
                 }
+                if (kvm_msr_list->indices[i] == MSR_IA32_SPEC_CTRL) {
+                    has_msr_spec_ctrl = true;
+                    continue;
+                }
             }
         }
 
@@ -1668,6 +1673,9 @@ static int kvm_put_msrs(X86CPU *cpu, int level)
     if (has_msr_xss) {
         kvm_msr_entry_add(cpu, MSR_IA32_XSS, env->xss);
     }
+    if (has_msr_spec_ctrl) {
+        kvm_msr_entry_add(cpu, MSR_IA32_SPEC_CTRL, env->spec_ctrl);
+    }
 #ifdef TARGET_X86_64
     if (lm_capable_kernel) {
         kvm_msr_entry_add(cpu, MSR_CSTAR, env->cstar);
@@ -1676,6 +1684,7 @@ static int kvm_put_msrs(X86CPU *cpu, int level)
         kvm_msr_entry_add(cpu, MSR_LSTAR, env->lstar);
     }
 #endif
+
     /*
      * The following MSRs have side effects on the guest or are too heavy
      * for normal writeback. Limit them to reset or full state updates.
@@ -2082,6 +2091,9 @@ static int kvm_get_msrs(X86CPU *cpu)
     if (has_msr_xss) {
         kvm_msr_entry_add(cpu, MSR_IA32_XSS, 0);
     }
+    if (has_msr_spec_ctrl) {
+        kvm_msr_entry_add(cpu, MSR_IA32_SPEC_CTRL, 0);
+    }
 
 
     if (!env->tsc_valid) {
@@ -2430,6 +2442,9 @@ static int kvm_get_msrs(X86CPU *cpu)
             } else {
                 env->mtrr_var[MSR_MTRRphysIndex(index)].base = msrs[i].data;
             }
+            break;
+        case MSR_IA32_SPEC_CTRL:
+            env->spec_ctrl = msrs[i].data;
             break;
         }
     }
