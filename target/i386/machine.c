vivier/qemu

Add RHEL 7 machine types
This commit adds all changes related to machine types applied in
qemu-kvm-rhev-2.1.2-16.el7.

Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

target-i386/machine.c
@@ -687,6 +687,25 @@ static const VMStateDescription vmstate_avx512 = {
     }
 };
 
+static bool vmstate_xsave_needed(void *opaque)
+{
+    /* The xsave state is already on the main "cpu" section */
+    return false;
+}
+
+static const VMStateDescription vmstate_xsave ={
+    .name = "cpu/xsave",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .minimum_version_id_old = 1,
+    .fields      = (VMStateField []) {
+    VMSTATE_UINT64_V(env.xcr0, X86CPU, 1),
+    VMSTATE_UINT64_V(env.xstate_bv, X86CPU, 1),
+    VMSTATE_YMMH_REGS_VARS(env.ymmh_regs, X86CPU, CPU_NB_REGS, 1),
+    VMSTATE_END_OF_LIST()
+    }
+};
+
 VMStateDescription vmstate_x86_cpu = {
     .name = "cpu",
     .version_id = 12,
@@ -833,6 +852,9 @@ VMStateDescription vmstate_x86_cpu = {
             .vmsd = &vmstate_avx512,
             .needed = avx512_needed,
         } , {
+            .vmsd = &vmstate_xsave,
+            .needed = vmstate_xsave_needed,
+        }, {
             /* empty */
         }
     }

M4cr0v/qemu-2.9.0

Conflicts (on 2.3 rebase):
	default-configs/ppc64-softmmu.mak
	hw/arm/Makefile.objs
	hw/i386/pc_piix.c
	hw/i386/pc_q35.c
	hw/ppc/spapr.c
	savevm.c - has to change shadow_bios tail to rcu list handling
	target-i386/machine.c - use xmm instead of ymmh register
(cherry picked from commit 61bf982cb6e977f0f77264f323b6d0bfd6129b5f)
Rebase notes (2.9.0):
- new header file for arm (upstream)
- query_hotpluggable_cpus renamed to has_hotpluggable_cpus (upstream)
- replace MAX_CPUMASK_BITS with max_cpus
- Adding rhel6-e1000.rom moved to Initial redhat commit
- Fixed conflict on cirrus_vga.c
Rebase notes (2.8.0):
- new "m->max_cpus = 288" in pc_q35_machine_options hw/i386/pc_q35.c
Rebase notes (2.7.0):
- Additional fsl-imx6.o sabrelito.o files in arm hw dir
Rebase notes (2.6.0):
- Changes in handling of some compat properties
- Fixes in x86_64 copmat models
- Added required devices for aarch64
- Fixes for ppc machine types
Rebase notes (2.5.0):
- changed cpu defaults structure
- chnaged cpu compat properties handling
- added fix for arm machine type
Rebase notes (2.4.0)
- Moved needed attribute (due to 5cd8cadae8db905afcbf877cae568c27d1d55a8a)
- Fixes to machine types changes
Merged patches (2.9.0)
- 8475d69 hw/arm/virt: Disable virtio-net-pci option ROM file loading
- 73fe1f6 Workaround rhel6 ctrl_guest_offloads machine type mismatch
- 21d32ca pc_piix: fix compat props typo for RHEL6 machine types
- 55a5002 compat: define HW_COMPAT_RHEL7_3
- 1b8e927 spapr: define pseries-rhel7.4.0 machine type
- cdb76ec hw/arm/virt: remove aarch64 rhel machine type
- 7dfa88b hw/arm/virt: create virt-rhel7.3.0 machine type
- 6894f91 hw/arm/virt: create virt-rhel7.4.0 machine type
- a9d2d39 x86: Split out options for the head rhel7 machine types
- fdafbdc x86: Create PC_RHEL7_3_COMPAT definition
- 3427c72 x86: Define pc-i440fx-rhel7.4.0
- aea20ab x86: Define pc-q35-rhel7.4.0
- 0185c0f x86: Remove downstream opteron rdtscp override
- 6b51073 fix abort in acpi_setup() since 2.8 with rhel6 machine 

target/i386/machine.c
@@ -927,6 +927,26 @@ static const VMStateDescription vmstate_mcg_ext_ctl = {
     }
 };
 
+static bool vmstate_xsave_needed(void *opaque)
+{
+    /* The xsave state is already on the main "cpu" section */
+    return false;
+}
+
+static const VMStateDescription vmstate_xsave ={
+    .name = "cpu/xsave",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .minimum_version_id_old = 1,
+    .needed = vmstate_xsave_needed,
+    .fields      = (VMStateField []) {
+    VMSTATE_UINT64_V(env.xcr0, X86CPU, 1),
+    VMSTATE_UINT64_V(env.xstate_bv, X86CPU, 1),
+    VMSTATE_YMMH_REGS_VARS(env.xmm_regs, X86CPU, CPU_NB_REGS, 1),
+    VMSTATE_END_OF_LIST()
+    }
+};
+
 VMStateDescription vmstate_x86_cpu = {
     .name = "cpu",
     .version_id = 12,
@@ -1054,6 +1074,7 @@ VMStateDescription vmstate_x86_cpu = {
         &vmstate_pkru,
 #endif
         &vmstate_mcg_ext_ctl,
+        &vmstate_xsave,
         NULL
     }
 };